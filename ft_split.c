a5fab81b23077426ed5a7b64db06419526b5302a
yait-nas <yait-nas@c3r2p6.1337.ma>
2024-07-24 00:55:23 +0100

    :)

--- ft_split.c
+++ ft_split.c
@@ -6,65 +6,75 @@
 /*   By: yait-nas <yait-nas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 13:45:30 by yait-nas          #+#    #+#             */
-/*   Updated: 2024/05/31 14:13:40 by yait-nas         ###   ########.fr       */
+/*   Updated: 2024/07/24 00:55:01 by yait-nas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	c_char(char *s, char c)
+static char	*word(char *s, int i, char c)
 {
-	int	i;
-
-	i = 0;
-	while (s[i] && s[i] != c)
-		i++;
-	return (i);
-}
-
-static char	*word(char *s, char **strings, int index, char c)
-{
-	int		i;
 	char	*p;
+	int		j;
 
-	i = 0;
-	p = malloc(sizeof(char) * (c_char(s, c) + 1));
+	
+	p = malloc(i + 1);
 	if (!p)
-	{
-		if (index)
-		{
-			while (index--)
-				free(strings[index]);
-		}
-		free(strings);
 		return (NULL);
-	}
-	while (s[i] != c && s[i])
+	j = 0;
+	while (*s && *s != c)
 	{
-		p[i] = s[i];
-		i++;
+		if (*s == '"')
+		{
+			p[j++] = *s;
+			s++;
+			while (*s && *s != '"')
+			{
+				p[j] = *s;
+				s++;
+				j++;
+			}
+		}
+		p[j] = *s;
+		j++;
+		s++;
 	}
-	p[i] = '\0';
+	p[j] = '\0';
 	return (p);
 }
 
 static void	to_be_continued(char **strings, char *s, char c)
 {
 	int	string_index;
+	int	i;
 
 	string_index = 0;
 	while (*s)
 	{
-		while (*s == c)
-			s++;
-		if (*s)
+		i = 0;
+		while (s[i] && s[i] != c)
 		{
-			strings[string_index] = word(s, strings, string_index, c);
-			if (!strings[string_index])
-				return ;
-			string_index++;
+			if (s[i] == '"')
+			{
+				i++;
+				while (s[i] && s[i] != '"')
+					i++;
+			}
+			i++;
 		}
-		while (*s && *s != c)
+		strings[string_index++] = word(s, i, c);
+		s = s + i;
+		/*while (*s && *s != c)
+		{
+			if (*s == '"')
+			{
+				s++;
+				while (*s && *s != '"')
+					s++;
+			}
+			s++;
+		}*/
+		if (*s)
 			s++;
 	}
 	strings[string_index] = NULL;
@@ -87,3 +97,19 @@ char	**ft_split(char	*s, char c)
 	to_be_continued(strings, s, c);
 	return (strings);
 }
+/*
+int	main()
+{
+	char	**ptr;
+
+	printf("count : %d\n", count_words_and_check("pwd   | echo \"|||\"", '|'));
+	ptr = ft_split("pwd        | echo \"||||||\"  ", '|');
+	if (ptr)
+	{
+		while (*ptr)
+		{
+			printf("%s\n", *ptr);
+			ptr++;
+		}
+	}
+}*/
